7fb6b2fd0b25767d3033c34974d5e3497e97ed0c
jabyum <jabyum@github.com>
2024-08-16 18:32:10 +0500

    interface

--- bot.py
+++ bot.py
@@ -2,8 +2,11 @@ import telebot
 import buttons as bt
 from geopy.geocoders import Photon
 import database as db
+# db.add_product("Бургер", 30000, "лучший бургер", 20, "https://menunedeli.ru/wp-content/uploads/2023/07/4523862E-973D-49D0-BE99-2609DDAA5CF4-933x700.jpeg")
+# db.add_product("Чизбургер", 35000, "лучший чизбургер", 20, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7lfdGQENwsxIWcpk80o43dRz2V8jN9pBK_w&s")
+# db.add_product("Хот-дог", 15000, "лучший хотдог", 0, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsfW388zWeoTBoYVtL5yJi85sJmFoVB3isLw&s")
 
-bot = telebot.TeleBot(token="TOKEN")
+bot = telebot.TeleBot(token="6849219345:AAFcDrJ-NC1FxsfoKqat672eAltYQe9RMpc")
 geolocator = Photon(user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36")
 
 @bot.message_handler(commands=["start"])
@@ -11,7 +14,7 @@ def start(message):
     user_id = message.from_user.id
     checker = db.check_user(user_id)
     if checker == True:
-        bot.send_message(user_id, "Главное меню")
+        bot.send_message(user_id, "Главное меню", reply_markup=bt.main_menu_bt())
     elif checker == False:
         bot.send_message(user_id, "Добро пожаловать в наш бот доставки!\n"
                                   "Напишите своё имя")
@@ -29,7 +32,7 @@ def get_number(message, name):
     if message.contact:
         phone_number = message.contact.phone_number
         bot.send_message(user_id, f"{name}, вы успешно зарегистрированы!\n"
-                                  "Выберите действие из меню")
+                                  "Выберите действие из меню", reply_markup=bt.main_menu_bt())
         db.add_user(name, phone_number, user_id)
     else:
         bot.send_message(user_id, "Отправьте свой номер через кнопку в меню",
@@ -42,5 +45,16 @@ def get_location(message):
         latitude = message.location.latitude
         address = geolocator.reverse((latitude, longitude)).address
         print(address)
+@bot.message_handler(content_types=["text"])
+def main_menu(message):
+    user_id = message.from_user.id
+    if message.text == "🍴Меню":
+        all_product = db.get_pr_id_name()
+        bot.send_message(user_id, "Меню", reply_markup=bt.products_in(all_product))
+    elif message.text == "🛒Корзина":
+        bot.send_message(user_id, "Ваша корзина:")
+    elif message.text == "❗️Отзыв":
+        bot.send_message(user_id, "Напишите текст вашего отзыва")
+
 
 bot.infinity_polling()
--- buttons.py
+++ buttons.py
@@ -10,3 +10,28 @@ def location_bt():
     button = types.KeyboardButton(text="Поделиться локацией", request_location=True)
     kb.add(button)
     return kb
+def main_menu_bt():
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    menu = types.KeyboardButton(text="🍴Меню")
+    cart = types.KeyboardButton(text="🛒Корзина")
+    feedback = types.KeyboardButton(text="❗️Отзыв")
+    kb.add(menu, cart, feedback)
+    return kb
+def products_in(all_products):
+    kb = types.InlineKeyboardMarkup(row_width=2)
+    # статичные кнопки
+    cart = types.InlineKeyboardButton(text="Корзина", callback_data="cart")
+    back = types.InlineKeyboardButton(text="Назад", callback_data="main_menu")
+    # динамичные кнопки
+    all_buttons = [types.InlineKeyboardButton(text=product[1], callback_data=f"prod_{product[0]}")
+                   for product in all_products]
+    kb.add(*all_buttons)
+    kb.row(cart)
+    kb.row(back)
+    return kb
+
+
+
+
+
+
